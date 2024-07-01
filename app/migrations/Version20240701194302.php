5d3475af82560473853ab98a372d4ac5f23c99ea
Julia Wróblewska <juliaw.wroblewska@student.uj.edu.pl>
2024-07-01 21:44:08 +0200

    w

--- app/migrations/Version20240701194302.php
+++ app/migrations/Version20240701194302.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240701191553 extends AbstractMigration
+final class Version20240701194302 extends AbstractMigration
 {
     public function getDescription(): string
     {
